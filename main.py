51e86108d0af9165715ddeeddbb0b31e5194f6d3
Tomás Rojas Castiglione <tomas.rojas.c@ug.uchile.cl>
2022-10-23 20:59:26 -0300

    main file working with sd interface working

--- main.py
+++ main.py
@@ -24,14 +24,17 @@ GND -> GND
 SCL -> Pin 5
 SDA -> Pin 4
 """
-"""
-from machine import SoftI2C, Pin, SoftSPI, UART, SPI, SDCard
+
+from machine import SoftI2C, Pin, SoftSPI, UART
 import utime
 from utils.micropyGPS import MicropyGPS
 from drivers.bmp280 import *
 from utils.bmp280 import calculate_altitude_from_pressure
 from drivers.mpu6050 import accel
 from utils.writeSD import read_values
+import os
+from drivers.sdcard import SDCard
+
 
 # I2C bus
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
@@ -46,24 +49,18 @@ bmp = BMP280(i2c)
 gpsModule = UART(2, baudrate=9600)
 gps_parser = MicropyGPS(location_formatting="dd")
 
-
-
 # SD card preliminar statements
-#spi_sd = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
-#sd = SDCard(spi_sd, Pin(27))
+spi_sd = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
+sd = SDCard(spi_sd, Pin(27))
+vfs = os.VfsFat(sd)
+os.mount(vfs, "/sd")
+
+
 while True:
     line = gpsModule.readline()
     utime.sleep(0.1)
     gps_parser.update_from_line(line)
     print(read_values(gps_parser, mpu, bmp))
    # print(mpu.get_values()["GyZ"])
-"""
-import machine
-import os
-from machine import SoftSPI, Pin
-from sdcard import SDCard
 
-spi_sd = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
-sd = SDCard(spi_sd, Pin(27))
-vfs = os.VfsFat(sd)
-os.mount(vfs, "/sd")
+
