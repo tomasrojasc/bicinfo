26a994fc1aa5943c1357b14d7cacd865521c2eb3
Tomás Rojas Castiglione <tomas.rojas.c@ug.uchile.cl>
2022-11-29 12:45:51 -0300

    adding tilt to mpu utils

--- utils/mpu6050.py
+++ utils/mpu6050.py
@@ -1,2 +1,15 @@
-def get_inclination():
-    pass
+import math
+
+
+def get_inclination(acx, acy, acz):
+    """
+    This function takes the acc in the different axes and returns the angles pitch, roll, yaw
+    :param acx: AcX from accelerometer
+    :param acy: AcY from accelerometer
+    :param acz: AcZ from accelerometer
+    :return: return angles pitch, roll, yaw in degrees
+    """
+    pitch = round(math.atan2(-acx, math.sqrt(acy ** 2 + acz ** 2)) * 57.3 * 100 / 45)  # +'%'
+    roll = round(math.atan2(-acy, math.sqrt(acx ** 2 + acz ** 2)) * 57.3 * 100 / 45)  # +'%'
+    yaw = round(math.atan2(-acz, math.sqrt(acx ** 2 + acy ** 2)) * 57.3 * 100 / 45)  # +'%'
+    return pitch, roll, yaw
